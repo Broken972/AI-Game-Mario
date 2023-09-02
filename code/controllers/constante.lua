48188638b4762c8a5df3f4bae8a3076c74e9ab1b
Broken972 <parfaite.pierre@gmail.com>
2023-09-02 17:54:15 +0200

    Add Commentaires Constantes

--- code/controllers/constante.lua
+++ code/controllers/constante.lua
@@ -1,61 +1,85 @@
--- Constantes --
-NOM_JEU = "Super Mario World (USA)" -- nom du jeu
-NOM_SAVESTATE = "debut.state" -- nom du savestate à charger
-NOM_FICHIER_POPULATION = "Generation IDGeneration.pop" -- IDGeneration sera remplacé par le nb de gen
+-- ########## Constantes pour le jeu ##########
 
--- Constantes pour l'interface formulaire
-TAILLE_FORM_W = 400	-- taille de la fenetre de l'interface formulaire 
-TAILLE_FORM_H = 400 -- taille de la fenetre de l'interface formulaire
+-- Nom du jeu et fichiers associés
+NOM_JEU = "Super Mario World (USA)"
+NOM_SAVESTATE = "debut.state"
+NOM_FICHIER_POPULATION = "Generation IDGeneration.pop" -- IDGeneration sera remplacé par le numéro de génération
 
--- Constantes pour l'interface graphique
-TAILLE_TILE = 16-- taille d'une tile DANS LE JEU
+-- ########## Constantes pour l'interface formulaire ##########
 
-TAILLE_VUE_W = TAILLE_TILE * 16 -- taille de ce que je vois le script
-TAILLE_VUE_H = TAILLE_TILE * 11 -- taille de ce que je vois le script
+-- Dimensions de la fenêtre de l'interface formulaire
+TAILLE_FORM_W = 400
+TAILLE_FORM_H = 400
 
+-- ########## Constantes pour l'interface graphique et le jeu ##########
 
+-- Taille d'une tuile (tile) dans le jeu
+TAILLE_TILE = 16
 
+-- Calcul de la taille de la vue du script basée sur la taille des tuiles
+TAILLE_VUE_W = TAILLE_TILE * 16
+TAILLE_VUE_H = TAILLE_TILE * 11
 
-TAILLE_CAMERA_W = 176 -- du jeu (en pixel)  (ça fait 25 tiles)
-TAILLE_CAMERA_H = 240 -- du jeu (en pixel) (ça fait 20 tiles)
-NB_TILE_W = TAILLE_VUE_W / TAILLE_TILE -- nombre de tiles scannée par le réseau de neurone en longueur (ça fait 25)
-NB_TILE_H = TAILLE_VUE_H / TAILLE_TILE -- nombre de tiles scannée par le réseau de neurone en largeur  (ça fait 20)
-NB_SPRITE_MAX = 11 -- dans SMW, il y a au maximum 12 sprites à l'écran en meme temps (en fait c'est 11+1 car 0 est un sprite), pour chaque type de sprite (à ne pas modifier)
+-- Dimensions de la caméra du jeu
+TAILLE_CAMERA_W = 176
+TAILLE_CAMERA_H = 240
 
-TAILLE_INPUT = 4 -- en pixel, uniquement pour l'affichage
-TAILLE_HIDDEN = 2 -- en pixel, uniquement pour l'affichage
-TAILLE_OUTPUT_W = 10 -- en pixel, rectangle button A, B, X, Y, Up, Down, Left, Right
-TAILLE_OUTPUT_H = 5 -- en pixel, rectangle button A, B, X, Y, Up, Down, Left, Right
-ENCRAGE_X_INPUT = 10 -- en pixel, uniquement pour l'affichage
-ENCRAGE_Y_INPUT = 40 -- en pixel, uniquement pour l'affichage
-ENCRAGE_X_HIDDEN = 100 -- en pixel, uniquement pour l'affichage
-ENCRAGE_Y_HIDDEN = 40 -- en pixel, uniquement pour l'affichage
-ENCRAGE_X_OUTPUT = 200 -- en pixel, uniquement pour l'affichage
-ENCRAGE_Y_OUTPUT = 40 -- en pixel, uniquement pour l'affichage
-ESPACE_Y_OUTPUT = TAILLE_OUTPUT_H + 2.5 -- en pixel, uniquement pour l'affichage (espace entre chaque output)
-NB_HIDDEN_PAR_LIGNE = 10 -- nombre de neurone hidden par ligne (affichage uniquement)
+-- Calcul du nombre de tuiles scannées par le réseau de neurones
+NB_TILE_W = TAILLE_VUE_W / TAILLE_TILE
+NB_TILE_H = TAILLE_VUE_H / TAILLE_TILE
 
-FITNESS_LEVEL_FINI = 1000000 -- quand le level est fini, la fitness devient ça
-NB_FRAME_RESET_BASE = 30 -- si pendant x frames la fitness n'augmente pas comparé à celle du début, on relance (le jeu tourne à 30 fps au cas où)
-NB_FRAME_RESET_PROGRES = 350 -- si il a eu un progrés (diff de la fitness au lancement) on laisse le jeu tourner un peu + longtemps avant le reset
-NB_NEURONE_MAX = 100000 -- pour le reseau de neurone, hors input et output
-NB_INPUT = NB_TILE_W * NB_TILE_H -- nb de neurones input, c'est chaque case du jeu en fait
-NB_OUTPUT = 8 -- nb de neurones output, c'est à dire les touches de la manette
-NB_INDIVIDU_POPULATION = 100 -- nombre d'individus créés quand création d'une nouvelle population
+-- Nombre maximal de sprites à l'écran en même temps dans SMW
+NB_SPRITE_MAX = 11
 
--- Constantes pour trier les espèces des populations
-EXCES_COEF = 0.50 -- Coefficient pour déterminer l'importance des gènes en excès lors du calcul de la distance génétique
-POIDSDIFF_COEF = 0.92 -- Coefficient pour déterminer l'importance des différences de poids lors du calcul de la distance génétique
-DIFF_LIMITE = 1.00 -- Limite de distance génétique pour déterminer si deux individus appartiennent à la même espèce
+-- ########## Constantes pour l'affichage du réseau de neurones ##########
 
--- Constantes pour les mutations
-CHANCE_MUTATION_RESET_CONNEXION = 0.20 -- Probabilité que le poids d'une connexion soit complètement réinitialisé lors d'une mutation
-POIDS_CONNEXION_MUTATION_AJOUT = 0.75 -- Poids ajouté à la mutation d'une connexion si CHANCE_MUTATION_RESET_CONNEXION n'est pas appliqué. La valeur peut être négative.
-CHANCE_MUTATION_POIDS = 0.90 -- Probabilité de mutation des poids des connexions
-CHANCE_MUTATION_CONNEXION = 0.80 -- Probabilité de mutation des connexions (ajout/suppression de connexions)
-CHANCE_MUTATION_NEURONE = 0.35 -- Probabilité de mutation des neurones (ajout/suppression de neurones)
+-- Tailles des différentes couches du réseau
+TAILLE_INPUT = 4
+TAILLE_HIDDEN = 2
+TAILLE_OUTPUT_W = 10
+TAILLE_OUTPUT_H = 5
 
--- doit correspondre aux inputs de la manette dans l'emulateur
+-- Ancrages pour les différents éléments du réseau
+ENCRAGE_X_INPUT = 10
+ENCRAGE_Y_INPUT = 40
+ENCRAGE_X_HIDDEN = 100
+ENCRAGE_Y_HIDDEN = 40
+ENCRAGE_X_OUTPUT = 200
+ENCRAGE_Y_OUTPUT = 40
+
+-- Autres paramètres d'affichage pour le réseau
+ESPACE_Y_OUTPUT = TAILLE_OUTPUT_H + 2.5
+NB_HIDDEN_PAR_LIGNE = 10
+
+-- ########## Constantes pour la fitness ##########
+
+-- Valeurs pour le calcul de la fitness
+FITNESS_LEVEL_FINI = 1000000
+NB_FRAME_RESET_BASE = 30
+NB_FRAME_RESET_PROGRES = 350
+NB_NEURONE_MAX = 100000
+NB_INPUT = NB_TILE_W * NB_TILE_H
+NB_OUTPUT = 8
+NB_INDIVIDU_POPULATION = 100
+
+-- ########## Constantes pour le tri des espèces ##########
+
+-- Coefficients pour le tri des espèces
+EXCES_COEF = 0.50
+POIDSDIFF_COEF = 0.92
+DIFF_LIMITE = 1.00
+
+-- ########## Constantes pour les mutations ##########
+
+-- Probabilités pour les différents types de mutations
+CHANCE_MUTATION_RESET_CONNEXION = 0.20
+POIDS_CONNEXION_MUTATION_AJOUT = 0.75
+CHANCE_MUTATION_POIDS = 0.90
+CHANCE_MUTATION_CONNEXION = 0.80
+CHANCE_MUTATION_NEURONE = 0.35
+
+-- ########## Correspondance des boutons avec l'émulateur ##########
+
 lesBoutons = {
 	{nom = "P1 A"},
 	{nom = "P1 B"},
@@ -66,16 +90,20 @@ lesBoutons = {
 	{nom = "P1 Left"},
 	{nom = "P1 Right"}
 }
-nbInnovation = 0 -- nombre d'innovation global pour les connexions, important pour le reseau de neurone
-fitnessMax = 0 -- fitness max atteinte 
-nbGeneration = 1 -- pour suivre on est à la cb de generation
-idPopulation = 1 -- quel id de la population est en train de passer dans la boucle
-marioBase = {} -- position de mario a la base ça va me servir pour voir si il avance de sa position d'origine / derniere pos enregistrée
+
+-- ########## Variables globales ##########
+
+-- Ces variables sont initialisées ici mais seront probablement modifiées pendant l'exécution du programme
+nbInnovation = 0
+fitnessMax = 0
+nbGeneration = 1
+idPopulation = 1
+marioBase = {}
 niveauFini = false
-lesAnciennesPopulation = {} -- stock les anciennes population
-nbFrame = 0 -- nb de frame actuellement
-nbFrameStop = 0 -- permettra de reset le jeu au besoin
-fitnessInit = 0 -- fitness à laquelle le reseau actuel commence est init
+lesAnciennesPopulation = {}
+nbFrame = 0
+nbFrameStop = 0
+fitnessInit = 0
 niveauFiniSauvegarde = false
 lesEspeces = {}
 laPopulation = {}
