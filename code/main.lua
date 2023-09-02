ec0a4829e55d7ec862b871cd5178f737fb7b6e2f
Broken972 <parfaite.pierre@gmail.com>
2023-09-02 17:08:07 +0200

    Add File Neurone

--- code/main.lua
+++ code/main.lua
@@ -5,7 +5,7 @@ dofile("./controllers/constante.lua")
 dofile("./controllers/population.lua")
 
 -- Loading neurone module
--- dofile("./controllers/neurone.lua")
+dofile("./controllers/neurone.lua")
 
 -- Loading connexion module
 -- dofile("./controllers/connexion.lua")
@@ -32,16 +32,7 @@ dofile("./controllers/population.lua")
 
 
 
--- Crée un nouveau neurone
-function newNeurone()
-    local neurone = {}
-    
-    neurone.valeur = 0         -- Valeur initiale du neurone
-    neurone.id = 0             -- Identifiant non initialisé si égal à 0, doit être égal à l'indice du neurone dans lesNeurones du réseau
-    neurone.type = ""          -- Type de neurone (input, hidden, output), doit être défini plus tard
-    
-    return neurone
-end
+
 
 
 -- Crée une nouvelle connexion
@@ -147,19 +138,6 @@ end
 
 
 
--- Ajoute un neurone à un réseau de neurones, utilisé seulement pour les neurones qui doivent exister
-function ajouterNeurone(unReseau, id, type, valeur)
-    if id == 0 then
-        console.log("La fonction ajouterNeurone ne doit pas être utilisée avec un id == 0")
-        return
-    end
-
-    local neurone = newNeurone()
-    neurone.id = id
-    neurone.type = type
-    neurone.valeur = valeur
-    table.insert(unReseau.lesNeurones, neurone)
-end
 
 
 
@@ -234,45 +212,6 @@ end
 
 
 
--- Ajoute un neurone (couche cachée uniquement) entre 2 neurones déjà connectés. Ne peut pas marcher
--- si il n'y a pas de connexion 
-function mutationAjouterNeurone(unReseau)
-    if #unReseau.lesConnexions == 0 then
-        console.log("Impossible d'ajouter un neurone entre 2 connexions si pas de connexion")
-        return nil
-    end
-    
-    if unReseau.nbNeurone == NB_NEURONE_MAX then
-        console.log("Nombre de neurone max atteint")
-        return nil
-    end
-
-    -- Randomisation de la liste des connexions
-    local listeRandom = {}
-    for i = 1, #unReseau.lesConnexions do
-        local pos = math.random(1, #listeRandom + 1)
-        table.insert(listeRandom, pos, i)
-    end
-
-    for _, indice in ipairs(listeRandom) do
-        local connexion = unReseau.lesConnexions[indice]
-        if connexion.actif then
-            -- Désactive la connexion existante
-            connexion.actif = false
-
-            -- Ajoute un nouveau neurone caché
-            unReseau.nbNeurone = unReseau.nbNeurone + 1
-            local indiceNeurone = unReseau.nbNeurone + NB_INPUT + NB_OUTPUT
-            ajouterNeurone(unReseau, indiceNeurone, "hidden", 1)
-
-            -- Ajoute deux nouvelles connexions
-            ajouterConnexion(unReseau, connexion.entree, indiceNeurone, genererPoids())
-            ajouterConnexion(unReseau, indiceNeurone, connexion.sortie, genererPoids())
-
-            break
-        end
-    end
-end
 
 
 
