602c6d2b2e2bca4291857ba30cd21f40788982a0
Broken972 <parfaite.pierre@gmail.com>
2023-09-02 18:00:42 +0200

    Add File Affichage

---
+++ code/controllers/affichage.lua
@@ -0,0 +1,12 @@
+-- dessine les informations actuelles
+function dessinerLesInfos(laPopulation, lesEspeces, nbGeneration)
+	gui.drawBox(0, 0, 256, 40, "black", "white")
+
+	gui.drawText(0, 4, "Generation " .. nbGeneration .. " Ind:" .. idPopulation .. " nb espece " .. 
+							#lesEspeces .. "\nFitness:" .. 
+							laPopulation[idPopulation].fitness .. " (max = " .. fitnessMax .. ")", "black")
+end
+
+function resetFitnessMax()
+    fitnessMax = 0
+end
--- code/main.lua
+++ code/main.lua
@@ -259,20 +259,11 @@ function traitementPause()
 	joypad.set(lesBoutons)
 end
 
-function resetFitnessMax()
-    fitnessMax = 0
-end
 
 
 
--- dessine les informations actuelles
-function dessinerLesInfos(laPopulation, lesEspeces, nbGeneration)
-	gui.drawBox(0, 0, 256, 40, "black", "white")
 
-	gui.drawText(0, 4, "Generation " .. nbGeneration .. " Ind:" .. idPopulation .. " nb espece " .. 
-							#lesEspeces .. "\nFitness:" .. 
-							laPopulation[idPopulation].fitness .. " (max = " .. fitnessMax .. ")", "black")
-end
+
 
 
 
