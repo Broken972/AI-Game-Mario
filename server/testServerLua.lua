0ab6c66d92844f1ddd8e26ce265b05dc35997549
Broken972 <parfaite.ppro@gmail.com>
2024-05-29 00:34:02 +0200

    refactor: Update code organization and improve readability

--- server/testServerLua.lua
+++ server/testServerLua.lua
@@ -1,16 +1,11 @@
--- Simple test script for HTTP connection in BizHawk
-local url = "http://127.0.0.1:8080"
+-- Charger la DLL en utilisant le nom complet de l'assembly
+local HttpHelperLibrary = luanet.load_assembly("HttpHelperLibrary")
 
--- Function to test HTTP connection
-function test_http_connection()
-    local response = comm.httpGet(url)
-    if response then
-        print("Connection successful!")
-        print("Server response: " .. response)
-    else
-        print("Failed to connect")
-    end
-end
+-- Importer la classe HttpHelper
+local HttpHelper = luanet.import_type("HttpHelperLibrary.HttpHelper")
 
--- Call the function to test connection
-test_http_connection()
+-- Effectuer la requête HTTP GET
+local response = HttpHelper.Get("http://127.0.0.1:8080")
+
+-- Afficher la réponse
+print(response)
